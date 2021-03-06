e02a71344f6c1bc772ac9db88a5f5cebc35147ae
unknown <damienbrt>
2021-03-06 14:39:36 +0100

    ajout full calendar

--- src/Controller/AgendaController.php
+++ src/Controller/AgendaController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Subject;
+use App\Repository\CalendarRepository;
 use App\Repository\SubjectRepository;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,8 +25,23 @@ class AgendaController extends AbstractController
     /**
      * @Route("/agenda", name="Agenda")
      */
-    public function Agenda(Request $request)
+    public function Agenda(Request $request, CalendarRepository $calendar)
     {
+        $events = $calendar->findAll();
+        $rdvs = [];
+        foreach ($events as $event){
+            $rdvs[]= [
+                'id' => $event->getId(),
+                'start' => $event->getStart()->format('Y-m-d H:i:s'),
+                'end' => $event->getEnd()->format('Y-m-d H:i:s'),
+                'title' => $event->getTitle(),
+                'description' => $event->getDescription(),
+                'backgroundColor' => $event->getBackgroundColor(),
+                'borderColor' => $event->getBorderColor(),
+                'textColor' => $event->getTextColor(),
+            ];
+        }
+        $data = json_encode($rdvs);
         /* Creation d'une matière dans la BDD
         $subject = new Subject();
         $subject->setName("MatièreTest")
@@ -35,10 +51,10 @@ class AgendaController extends AbstractController
         $this->em->flush();
         dump($subject);*/
 
-        $subject = $this->repository->findAll();
+        //$subject = $this->repository->findAll();
         //$subject = $this->repository->findOneBy(['TT_hour' => 123]);
         //$subject = $this->repository->findAllByHour();
-
-        return $this->render('Agenda/Agenda.html.twig', ['subjects' => $subject]);
+//, ['subjects' => $subject]
+        return $this->render('Agenda/Agenda.html.twig',compact('data'));
     }
 }
