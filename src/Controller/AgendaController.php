471954820912e6c84e047857998942449e5e13d2
MartialLaurens <37068941+MartialLaurens@users.noreply.github.com>
2021-03-08 11:21:53 +0100

    nettoyage controler agenda

--- src/Controller/AgendaController.php
+++ src/Controller/AgendaController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Subject;
 use App\Repository\CalendarRepository;
-use App\Repository\SubjectRepository;
-use Doctrine\Persistence\ObjectManager;
 use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,21 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class AgendaController extends AbstractController
 {
-    /**
-     * @var SubjectRepository
-     */
-    private $repository;
-
-    /**
-     * @var em
-     */
-    private $em;
-
-    public function __construct(SubjectRepository $Reposit, ObjectManager  $em)
-    {
-        $this->repository = $Reposit;
-        $this->em = $em;
-    }
 
     /**
      * @Route("/agenda", name="Agenda")
@@ -50,20 +32,6 @@ class AgendaController extends AbstractController
             ];
         }
         $data = json_encode($rdvs);
-        /* Creation d'une matière dans la BDD
-        $subject = new Subject();
-        $subject->setName("MatièreTest")
-            ->setTTHour(10);
-        $this->em = $this->getDoctrine()->getManager();
-        $this->em->persist($subject);
-        $this->em->flush();
-        dump($subject);*/
-
-        $subject = $this->repository->findAll();
-        $this->em->flush();
-        //$subject = $this->repository->findOneBy(['TT_hour' => 123]);
-        //$subject = $this->repository->findAllByHour();
-//, ['subjects' => $subject]
         return $this->render('Agenda/Agenda.html.twig',compact('data'));
     }
 }
