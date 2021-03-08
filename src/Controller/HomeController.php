c610922096f5f1795a77f669a898954708dbdc70
MartialLaurens <37068941+MartialLaurens@users.noreply.github.com>
2021-03-08 11:22:25 +0100

    Ajout affichage home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +13,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * @var SubjectRepository
+     */
+    private $repository;
+
+    /**
+     * @var em
+     */
+    private $em;
+
+    public function __construct()
+    {
+    }
+
+    /**
      * @Route("/home", name="home")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig');
-}
+        //Creation d'un user dans la BDD
+        $User = new User();
+
+        $this->repository = $this->getDoctrine()->getRepository(User::class);
+        $this->em = $this->getDoctrine()->getManager();
+
+        /*
+        $this->em->persist($User);
+        $this->em->flush();
+        dump($User);*/
+
+        //$User = $this->repository->findOneBy(['TT_hour' => 123]);
+        $User = $this->repository->findAll();
+        $this->em->flush();
+
+        //return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', ['user' => $User]);
+    }
 }
